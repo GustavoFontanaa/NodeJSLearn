cd70ab7f5a12a3cadf5fd35fe4ae9df928e91b13
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-14 19:54:06 -0300

    using 'WHERE' to retrieve data

--- 8_NODE_E_MYSQL/index.js
+++ 8_NODE_E_MYSQL/index.js
@@ -58,6 +58,22 @@ app.get('/books', (req, res) => {
 
 })
 
+app.get('/books/:id', (req, res) => {
+    const id = req.params.id
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    conn.query(sql, function(err, data){
+        if(err) {
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+
+        res.render('book', { book })
+    })
+})
+
 const conn = mysql.createConnection(
     {
         host: 'localhost',
