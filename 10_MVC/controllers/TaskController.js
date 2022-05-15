972547942a63f515f3f8d3f05e324b9abed3d5e7
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-15 20:48:50 -0300

    redeeming items + CSS

--- 10_MVC/controllers/TaskController.js
+++ 10_MVC/controllers/TaskController.js
@@ -5,8 +5,11 @@ module.exports = class TaskController {
     res.render('tasks/create')
   }
 
-  static showTasks(req, res) {
-    res.render('tasks/all')
+  static async showTasks(req, res) {
+
+    const tasks = await Task.findAll({raw: true})
+
+    res.render('tasks/all', {tasks})
   }
 
   static async createTaskSave(req, res) {
