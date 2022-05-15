231d37be1a13af15010fc6f04c056a2adacb962e
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-15 20:09:19 -0300

    Create Model

--- 10_MVC/db/conn.js
+++ 10_MVC/db/conn.js
@@ -12,4 +12,4 @@ try {
   console.error('Não foi possível conectar:', error)
 }
 
-exports.default = sequelize
+module.exports = sequelize
--- 10_MVC/index.js
+++ 10_MVC/index.js
@@ -1,21 +1,28 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const app = express()
+const app = express();
 
-const conn = require('./db/conn')
+const conn = require("./db/conn");
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+const Task = require("./models/Task");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(
   express.urlencoded({
     extended: true,
-  }),
-)
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.listen(3000)
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
