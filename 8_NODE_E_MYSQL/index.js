a6e79cc185e858b3a702126ab3ab72ff3f7d987a
Gustavo Fontana <gustavoasf77@gmail.com>
2022-05-14 23:04:34 -0300

    Connection Pool - Cache improvement

--- 8_NODE_E_MYSQL/index.js
+++ 8_NODE_E_MYSQL/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mysql = require('mysql')
+const pool = require('./db/conn')
 
 const app = express()
 
@@ -28,7 +28,7 @@ app.post('/books/insertbook', (req, res) => {
 
     const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
 
-    conn.query(sql, function(err) {
+    pool.query(sql, function(err) {
         if(err) {
             console.log(err)
             return
@@ -41,7 +41,7 @@ app.post('/books/insertbook', (req, res) => {
 app.get('/books', (req, res) => {
     const sql = "SELECT * FROM books"
 
-    conn.query(sql, function (err, data) {
+    pool.query(sql, function (err, data) {
         
         if(err) {
             console.log(err)
@@ -62,7 +62,7 @@ app.get('/books/:id', (req, res) => {
     const id = req.params.id
     const sql = `SELECT * FROM books WHERE id = ${id}`
 
-    conn.query(sql, function(err, data){
+    pool.query(sql, function(err, data){
         if(err) {
             console.log(err)
             return
@@ -78,7 +78,7 @@ app.get('/books/edit/:id', (req, res) => {
     const id = req.params.id
     const sql = `SELECT * FROM books WHERE id = ${id}`
 
-    conn.query(sql, function (err, data){
+    pool.query(sql, function (err, data){
 
         if(err) {
             console.log(err)
@@ -99,7 +99,7 @@ app.post('/books/updatebook', (req, res) => {
 
     const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
 
-    conn.query(sql, function (err){
+    pool.query(sql, function (err){
 
         if(err) {
             console.log(err)
@@ -116,7 +116,7 @@ app.post('/books/remove/:id', (req, res) => {
 
     const sql = `DELETE FROM books WHERE id = ${id}`
 
-    conn.query(sql, function (err){
+    pool.query(sql, function (err){
 
         if(err) {
             console.log(err)
@@ -127,23 +127,4 @@ app.post('/books/remove/:id', (req, res) => {
     })
 })
 
-
-
-const conn = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'nodemysql'
-    }
-)
-
-conn.connect(function (err) {
-    if (err) {
-        console.log(err)
-    }
-
-    console.log('Conectou ao MySQL!')
-
-    app.listen(3000)
-})
+app.listen(3000)
